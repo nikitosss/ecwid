9db714e67f5cd5fb49dce82f0e07ad1a768e425d
nikitosss <nikitosss@users.noreply.github.com>
2020-07-10 18:49:30 +0000

    deploy: ef27b397515f8b41cee03d646c441a9ba167e587

---
+++ precache-manifest.06c005237abeb473ebabec2cb2cd7a25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b1f6a7691b76d8460243bee5c78181",
+    "url": "/ecwid/index.html"
+  },
+  {
+    "revision": "b8c0ee97593d14608530",
+    "url": "/ecwid/static/css/3.37c6aa96.chunk.css"
+  },
+  {
+    "revision": "315f82864a7444282c1f",
+    "url": "/ecwid/static/css/main.1f61d72b.chunk.css"
+  },
+  {
+    "revision": "b13d2d128ddd8e78ca3d",
+    "url": "/ecwid/static/js/2.e1ec44f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecwid/static/js/2.e1ec44f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c0ee97593d14608530",
+    "url": "/ecwid/static/js/3.f1065ec2.chunk.js"
+  },
+  {
+    "revision": "315f82864a7444282c1f",
+    "url": "/ecwid/static/js/main.ce883d1d.chunk.js"
+  },
+  {
+    "revision": "a6182bdcff9372daeeb8",
+    "url": "/ecwid/static/js/runtime-main.7063ef6d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ecwid/precache-manifest.6b9a38db78987aa49bcf540320dbcdc0.js"
+  "/ecwid/precache-manifest.06c005237abeb473ebabec2cb2cd7a25.js"
 );
 
 self.addEventListener('message', (event) => {
